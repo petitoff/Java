fbbc366c95df07a95754fcd45ea811b451d49368
Błażej Domagała <petitoff@outlook.com>
2023-03-13 14:18:10 +0100

    Test exceptions

--- Unit test/HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ Unit test/HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -1,12 +1,13 @@
 package com.healthycoderapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BMICalculatorTest {
     @Test
-    void Should_ReturnTrue_When_DietRecommended() {
+    void should_ReturnTrue_When_DietRecommended() {
         // given
         double weight = 89.0;
         double height = 1.72;
@@ -19,7 +20,7 @@ class BMICalculatorTest {
     }
 
     @Test
-    void Should_ReturnFalse_When_DietNotRecommended() {
+    void should_ReturnFalse_When_DietNotRecommended() {
         // given
         double weight = 50.0;
         double height = 1.92;
@@ -29,5 +30,18 @@ class BMICalculatorTest {
 
         // then
         assertFalse(recommended);
+    }
+
+    @Test
+    void should_ThrowArithmeticException_When_HeightZero() {
+        // given
+        double weight = 50.0;
+        double height = 0;
+
+        // when
+        Executable executable = () -> BMICalculator.isDietRecommended(weight, height);
+
+        // then
+        assertThrows(ArithmeticException.class, executable);
     }
 }
