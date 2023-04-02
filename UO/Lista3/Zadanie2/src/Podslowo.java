4aef81693f90d665260fb40eb842b5dc92d0e1e9
Błażej Domagała <petitoff@outlook.com>
2023-04-02 22:18:52 +0200

    Added substring checking functionality using charAt() method

---
+++ UO/Lista3/Zadanie2/src/Podslowo.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Podslowo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj pierwsze słowo: ");
+        String s1 = scanner.nextLine();
+
+        System.out.print("Podaj drugie słowo: ");
+        String s2 = scanner.nextLine();
+
+        if (isSubstring(s1, s2)) {
+            System.out.println(s1 + " jest podsłowem " + s2);
+        } else {
+            System.out.println(s1 + " nie jest podsłowem " + s2);
+        }
+    }
+
+    public static boolean isSubstring(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        for (int i = 0; i <= m - n; i++) {
+            int j;
+            for (j = 0; j < n; j++) {
+                if (s2.charAt(i + j) != s1.charAt(j)) {
+                    break;
+                }
+            }
+            if (j == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
