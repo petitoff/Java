bf7019eb93baeb437c2dc918aae4e76617cf1507
Karolina Dbaj <k.dbaj@outlook.com>
2023-04-03 08:39:52 +0200

    Update Podslowo.java

--- UO/Lista3/Zadanie2/src/Podslowo.java
+++ UO/Lista3/Zadanie2/src/Podslowo.java
@@ -10,17 +10,16 @@ public class Podslowo {
         System.out.print("Podaj drugie słowo: ");
         String s2 = scanner.nextLine();
 
-        if (isSubstring(s1, s2)) {
+        if (isSubstringContained(s1, s2)) {
             System.out.println(s1 + " jest podsłowem " + s2);
         } else {
             System.out.println(s1 + " nie jest podsłowem " + s2);
         }
     }
 
-    public static boolean isStringContained(String s1, String s2) {
+    public static boolean isSubstringContained(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-
         for (int i = 0; i <= m - n; i++) {
             int j;
             for (j = 0; j < n; j++) {
