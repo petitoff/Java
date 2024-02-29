a83aa64ba7eef8fb586a8e1a7f62bf0cbb50411e
Błażej Domagała <petitoff@outlook.com>
2024-02-29 16:22:10 +0100

    added new implementation of czyPodslowo

--- UO/Lista3/Zadanie2/src/Podslowo.java
+++ UO/Lista3/Zadanie2/src/Podslowo.java
@@ -10,11 +10,37 @@ public class Podslowo {
         System.out.print("Podaj drugie słowo: ");
         String s2 = scanner.nextLine();
 
-        if (isSubstringContained(s1, s2)) {
+        if (czyPodslowo(s1, s2)) {
             System.out.println(s1 + " jest podsłowem " + s2);
         } else {
             System.out.println(s1 + " nie jest podsłowem " + s2);
         }
+    }
+
+    public static boolean czyPodslowo(String slowo, String podslowo) {
+        // Sprawdzenie, czy podslowo jest dłuższe niż slowo. Jeśli tak, to nie może być jego podciągiem.
+        if (podslowo.length() > slowo.length()) {
+            return false;
+        }
+
+        // Przeszukujemy każdy możliwy początek podciągu w slowo.
+        for (int i = 0; i <= slowo.length() - podslowo.length(); i++) {
+            int j;
+            // Dla każdego początku porównujemy znaki jeden po drugim.
+            for (j = 0; j < podslowo.length(); j++) {
+                if (slowo.charAt(i + j) != podslowo.charAt(j)) {
+                    break;
+                }
+            }
+            // Jeśli doszliśmy do końca podslowo bez przerwania pętli,
+            // oznacza to, że znaleźliśmy pełne dopasowanie.
+            if (j == podslowo.length()) {
+                return true;
+            }
+        }
+
+        // Jeśli nie znaleziono dopasowania, zwracamy false.
+        return false;
     }
 
     public static boolean isSubstringContained(String substring, String string) {
